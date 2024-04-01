056cbcfeab5be9e01fdf22efbca471b344432e1d
James Wadley <86320001+w4dd325@users.noreply.github.com>
2024-04-01 14:40:05 +0100

    add files

---
+++ Advanced Node and Express/3.Set_up_Passport.js
@@ -0,0 +1,38 @@
+//The server.js file should look like this: 
+//create a .env file and add SESSION_SECRET (.env should NOT has a prefix)
+
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.route('/').get((req, res) => {
+  res.render('index', { title: 'Hello', message: 'Please log in' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
